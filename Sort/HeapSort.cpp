326e41e9650b7ab9cfe8112d295f284652fceb02
VyLT <145197778+VyLoTe@users.noreply.github.com>
2025-07-25 20:28:55 +0700

    use recursive

--- Sort/HeapSort.cpp
+++ Sort/HeapSort.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <class T>
-void Heapify(T* a, int i, int n) {
+vector<int> a;
+vector<int> b;
+
+void Heapify(int i, int n) {
     int kt = 0;
     int max;
     int j = i;
-    while(kt == 0 && j <=n/2) 
+    while(kt == 0 && 2 * j + 1 < n) 
     {
-        if (n == 2*j) max = 2*j;
+        if (2 * j + 2 >= n) max = 2 * j + 1;  
         else {
-            if(a[2*j] <= a[2*j+1]) max = 2*j+1;
-            else max = 2*j;
+            if(a[2*j+1] < a[2*j+2]) max = 2*j+2;
+            else max = 2*j+1;
         }
         if (a[j] < a[max]) {
             swap(a[j],a[max]);
@@ -20,23 +22,25 @@ void Heapify(T* a, int i, int n) {
         else kt = 1;
     }
 }
-template <class T>
-void HeapSort(T *a,int n) {
-    int i;
-    for (i = (n-1)/2; i >=0; --i) 
-        Heapify(a, i, n-1);
 
-    for (i = n-1; i >= 1; --i) {
+void HeapSort(int n) {
+    for (int i = n/2 -1; i >= 0; --i)
+        Heapify(i, n-1);
+
+    for (int i = n-1; i >= 1; --i) {
+        b.push_back(a[0]);
         swap(a[0], a[i]); 
-        Heapify(a, 0, i-1);
+        Heapify(0, i-1);
     }
+    b.push_back(a[0]);
 }
 
 int main() {
-    int a[] = {12, 13, 9, 25, 7, 31, 3, 37, 14, 22, 15};
-    int n = sizeof(a)/sizeof(int);
-    HeapSort(a, 11);
-    for (auto z : a) cout << z << ' ';
+    int c[] = {12, 13, 9, 25, 7, 31, 3, 37, 14, 22, 15};
+    int n = sizeof(c)/sizeof(int);
+    for (int i = 0; i < n; ++i) a.push_back(c[i]);
+    HeapSort(n);
+    for (auto z : b) cout << z << ' ';
 
     return 0;
 }
